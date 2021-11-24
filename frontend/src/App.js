bc9497d6f3405d7468205c026494d059a93d7808
Guillaume Gibert <guillaume.gibe@gmail.com>
2021-11-24 18:55:35 +0100

    galère sur l'api nasa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 var urls = [
   "https://jsonplaceholder.typicode.com/posts/1",
-   "https://api.nasa.gov/planetary/apod?api_key=g5OQbgckhrUaLRMKNcNVRYtKuCmj41mvfTLhRe3T"
+   "https://api.nasa.gov/planetary/apod?api_key=g5OQbgckhrUaLRMKNcNVRYtKuCmj41mvfTLhRe3T",
+   "https://randomuser.me/api/"
 ]
 function checkStatus(response) {
   if (response.ok) {
@@ -23,6 +24,7 @@ class App extends React.Component {
     this.state={
       api1:[],
       api2:[],
+      api3:[],
       isLoaded: false,
     }
   }
@@ -38,10 +40,12 @@ class App extends React.Component {
       .then((result) => {
         const result_api1 = result[0];
         const result_api2= result[1];
+        const result_api3= result[2];
         this.setState({
           isLoaded: true,
           api1:result_api1,
           api2:result_api2,
+          api3:result_api3,
         })
         console.log(this.state);
       })
@@ -54,6 +58,7 @@ class App extends React.Component {
           <h1>La dernière blague : </h1>
           {this.state.api1.body}
           {this.state.api2.copyright}
+          {this.state.api3.results.map(=>(<h1 key={0}>{gender}</h1>))}
         </div>
       </div>
     );
