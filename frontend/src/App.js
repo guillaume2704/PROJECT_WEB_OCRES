5b508d407efbe698369532467f75cef611a2f2ce
Guillaume Gibert <guillaume.gibe@gmail.com>
2021-11-24 18:02:12 +0100

    2api test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,63 @@
 import React from 'react';
 import './App.css';
 
+var urls = [
+  "https://jsonplaceholder.typicode.com/posts/1",
+   "https://api.nasa.gov/planetary/apod?api_key=g5OQbgckhrUaLRMKNcNVRYtKuCmj41mvfTLhRe3T"
+]
+function checkStatus(response) {
+  if (response.ok) {
+      return Promise.resolve(response);
+  } else {
+      return Promise.reject(new Error(response.statusText));
+  }
+}
+
+function parseJSON(response) {
+return response.json();
+}
+
 class App extends React.Component {
-state = {
-  post : {}
-}
+  constructor(props) {
+    super(props);
+    this.state={
+      api1:[],
+      api2:[],
+      isLoaded: false,
+    }
+  }
 
-componentDidMount(){
-  fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then((response) => {
-    return response.json()
-  })
-  .then((result) => {
-    this.setState({post: result})
-  })
 
-}
-render() {
-  return (
-    <div className="App">
-      <h1>La dernière blague : </h1>
-      {this.state.post.body}
-    </div>
-  );
-}
+  componentDidMount() {
+        Promise.all(urls.map(url =>
+            fetch(url)
+                .then(checkStatus)  // check the response of our APIs
+                .then(parseJSON)    // parse it to Json
+                .catch(error => console.log('There was a problem!', error))
+        ))
+      .then((result) => {
+        const result_api1 = result[0];
+        const result_api2= result[1];
+        this.setState({
+          isLoaded: true,
+          api1:result_api1,
+          api2:result_api2,
+        })
+        console.log(this.state);
+      })
+
+  }
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <h1>La dernière blague : </h1>
+          {this.state.api1.body}
+          {this.state.api2.copyright}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
